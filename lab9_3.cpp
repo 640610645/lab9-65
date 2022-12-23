ec5255016752d9b59b247ffef86b5c433ae555a2
640610645 <nontapan_chanadee@cmu.ac.th>
2022-12-23 15:59:30 +0700

    lab8 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,30 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,height,bounty;
+    string c;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100) c = "Chopper";
+        else if(height < 180) c = "Usopp";
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1.1e9) c = "Zoro";
+            else  c = "Sanji";
+        }
+    }else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 5*10e7) c = "Jinbe";
+        else c = "Franky";
+
+    }else c = "Brook";
+    cout << "Your character = " << c;
+    return 0;
+}
